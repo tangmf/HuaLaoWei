7e3c4e391cc6ab6ae2af33dfd7c3b8c6953750a2
unknown <flemingsiow@gmail.com>
2025-04-23 15:18:58 +0800

    Updated DB schema w/ new seeds

---
+++ database/schema/subzones.sql
@@ -0,0 +1,6 @@
+CREATE TABLE subzones (
+    subzone_id SERIAL PRIMARY KEY,
+    planning_area_id INTEGER REFERENCES planning_areas(planning_area_id),
+    name VARCHAR(100) NOT NULL,
+    geom GEOGRAPHY(MULTIPOLYGON, 4326) -- Spatial boundary of the subzone
+);
